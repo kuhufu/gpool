6b9eb8dfbfdf2bbce03407e8f5bb4a77c96137a9
kuhufu <huboemail@yeah.net>
2019-12-05 14:48:51 +0800

    update

--- task.go
+++ task.go
@@ -3,22 +3,6 @@ package gpool
 type RunTaskFunc func()
 type CallTaskFunc func() interface{}
 
-func NewRunTask(fn func(), p *Pool) task {
-	return &RunTask{
-		fn:   fn,
-		done: make(chan interface{}),
-		err:  nil,
-	}
-}
-
-func NewCallTask(fn func() interface{}, p *Pool) task {
-	return &CallTask{
-		fn:   fn,
-		done: make(chan interface{}, 1),
-		err:  nil,
-	}
-}
-
 type item interface {
 	run()
 	discard()
